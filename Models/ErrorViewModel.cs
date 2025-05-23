194da63b4a41f9e8d7cf6618bcfb0edcfdbe0c6e
omartins-zs <gabrielmatheus209@gmail.com>
2025-05-23 00:05:32 -0300

    Iniciando Projeto Sistema de Reserva de Horários

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaReservaHorarios.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
