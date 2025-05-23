6c78aa122b946be6408bca1e9c5c416791e852f3
omartins-zs <gabrielmatheus209@gmail.com>
2025-05-23 16:52:28 -0300

    Criando o Model ApplicationUser do Identity

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace SistemaReservaHorarios.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Nome { get; set; }
+
+        // Você pode colocar mais campos se quiser, como:
+        // public string CPF { get; set; }
+        // public string Endereco { get; set; }
+
+        public ICollection<Reserva> Reservas { get; set; }
+    }
+}
