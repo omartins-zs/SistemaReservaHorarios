ee978a8f0153db75a4882471a9a7e712a70c3439
omartins-zs <gabrielmatheus209@gmail.com>
2025-05-23 16:27:33 -0300

    Add App DbContext Contexto do BD(Ponte/Conexao BD)

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaReservaHorarios.Models;
+
+using SistemaReservaHorarios.Models;
+using System.Collections.Generic;
+
+namespace SistemaReservaHorarios.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<HorarioDisponivel> HorariosDisponiveis { get; set; }
+        public DbSet<Reserva> Reservas { get; set; }
+    }
+}
