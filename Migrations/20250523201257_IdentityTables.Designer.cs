9bcd1e94158486bc783e446fa4325a8c23a621eb
omartins-zs <gabrielmatheus209@gmail.com>
2025-05-23 17:13:45 -0300

    Criando as Migrations de Identity

--- Migrations/20250523201257_IdentityTables.Designer.cs
+++ Migrations/20250523201257_IdentityTables.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SistemaReservaHorarios.Data;
 
@@ -11,9 +12,11 @@ using SistemaReservaHorarios.Data;
 namespace SistemaReservaHorarios.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250523201257_IdentityTables")]
+    partial class IdentityTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
