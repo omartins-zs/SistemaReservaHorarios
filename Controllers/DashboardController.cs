86a0ecb84d9c32a5510124b615df01780ff93de6
omartins-zs <gabrielmatheus209@gmail.com>
2025-05-23 18:31:18 -0300

    Criando Dashboard Controller

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaReservaHorarios.Controllers
+{
+    public class DashboardController : Controller
+    {
+        [Authorize(Roles = "Admin")]
+        public IActionResult Admin()
+        {
+            return View(); // Página só para Admin
+        }
+
+        [Authorize(Roles = "Cliente")]
+        public IActionResult Cliente()
+        {
+            return View(); // Página só para Cliente
+        }
+
+        [Authorize] // Qualquer usuário logado
+        public IActionResult Index()
+        {
+            return View(); // Página para qualquer usuário autenticado
+        }
+    }
+}
